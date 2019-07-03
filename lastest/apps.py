5c9d260632f2f51cb0a10b6c22d5f638b1acb3bb
Goto-Namj <45956673+Goto-Namj@users.noreply.github.com>
2019-07-03 15:14:00 +0900

    분리했어요

--- lastest/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LastestConfig(AppConfig):
-    name = 'lastest'
--- lastest/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class User(models.Model):
-    name = models.CharField(max_length=10)
-    idid = models.CharField(max_length=16)
-    passpass = models.CharField(max_length=20)
