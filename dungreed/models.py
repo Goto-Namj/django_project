08da8813a01f31d8b15487d3e62c2b4132a4c134
Goto-Namj <45956673+Goto-Namj@users.noreply.github.com>
2019-07-15 21:20:54 +0900

    계산기 모델 제작

--- dungreed/models.py
+++ dungreed/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class damage(models.Model):
+    weapon_damage_min = models.CharField(max_length=3)
+    weapon_damage_max = models.CharField(max_length=3)
+    power = models.CharField(max_length=3)
+    critical_chance = models.CharField(max_length=3)
+    critical_damage = models.CharField(max_length=3)
+    fix_damage = models.CharField(max_length=2)
+    attack_speed = models.CharField(max_length=4)
+    
