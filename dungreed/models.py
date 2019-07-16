e7e54ec1c34d82851a2ddfdc358f33e996c21420
Goto-Namj <45956673+Goto-Namj@users.noreply.github.com>
2019-07-16 17:41:31 +0900

    쓸모없는 모델을 지웠다

--- dungreed/models.py
+++ dungreed/models.py
@@ -1,12 +1,3 @@
 from django.db import models
 
 
-class damage(models.Model):
-    weapon_damage_min = models.CharField(max_length=3)
-    weapon_damage_max = models.CharField(max_length=3)
-    power = models.CharField(max_length=3)
-    critical_chance = models.CharField(max_length=3)
-    critical_damage = models.CharField(max_length=3)
-    fix_damage = models.CharField(max_length=2)
-    attack_speed = models.CharField(max_length=4)
-    
