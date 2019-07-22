8e0ea958c85a56177b16fc6ed52f446a934c3329
Goto-Namj <45956673+Goto-Namj@users.noreply.github.com>
2019-07-22 19:24:54 +0900

    정상적이지 않은 값 처리

--- dungreed/views.py
+++ dungreed/views.py
@@ -4,11 +4,10 @@ from .models import Calc
 
 
 def dps_test(request):
-    check_list = ['1','2','3','4','5','6','7']
-    for i in check_list:
-        if not request.GET.get(i):
-            return render(request, 'dungreed/dps.html')
-    datas=list(map(float,request.GET.dict().values()))
+    try:
+        datas=list(map(float,request.GET.dict().values()))
+    except:
+        return render(request, 'dungreed/dps.html', {'err':'에러'})
     q = Calc().basic(datas)
     print(datas)
     return render(request, 'dungreed/dps.html', {'q':q})
