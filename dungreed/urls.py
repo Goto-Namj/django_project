82a10d74a2994443cf5ca46476f507c6514eecab
Goto-Namj <45956673+Goto-Namj@users.noreply.github.com>
2019-09-14 20:42:59 +0900

    메인 페이지 목록 path지정

--- dungreed/urls.py
+++ dungreed/urls.py
@@ -4,5 +4,22 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.dps_test, name='dps_test'),
+    path('',
+        views.main_page,
+        name='main_page'),
+    path('dps_test/',
+        views.dps_test,
+        name='dps_test'),
+    path('item_dictionary/',
+        views.dps_test,
+        name='item_dictionary'),
+    path('free_bulletin_board/',
+        views.dps_test,
+        name='free_bulletin_board'),
+    path('guide/',
+        views.dps_test,
+        name='guide'),
+    path('simulator/',
+        views.dps_test,
+        name='simulator'),
 ]
