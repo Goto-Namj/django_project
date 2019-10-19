802cb218a6329dbdb3ec2add9f6943af7a6d9af6
Goto-Namj <45956673+Goto-Namj@users.noreply.github.com>
2019-10-20 00:24:46 +0900

    만들어버린 슬라이더

--- dungreed/static/js/item_js.js
+++ dungreed/static/js/item_js.js
@@ -16,4 +16,30 @@ $(document).ready(function() {
         $("."+event.target.className+"_choose").addClass('item_element_choose');
     });
 
+
+    var vl1 = 0;
+    var vl2 = 100;
+    $("#slider").slider({
+        range: true,
+        values: [vl1,vl2],
+        step: 1,
+        min: 0,
+        max: 100,
+        slide: function( event, ui ) {
+            $( "#ipt1" ).val( "$" + ui.values[0] );
+            $( "#ipt2" ).val( "$" + ui.values[1] );
+        }
+    });
+    
+    $("#ipt1").change(function () {
+        var value1 = this.value.substring(1);
+        vl1 = parseInt(value1);
+        $("#slider").slider("values", [vl1,vl2]);
+    });
+
+    $("#ipt2").change(function () {
+        var value2 = this.value.substring(1);
+        vl2 = parseInt(value2);
+        $("#slider").slider("values", [vl1,vl2]);
+    });
 });
