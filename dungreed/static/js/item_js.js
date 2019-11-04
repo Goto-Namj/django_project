b2a58891c64877d15b9d4eb466ca76cddc29736c
Goto-Namj <45956673+Goto-Namj@users.noreply.github.com>
2019-11-04 19:45:05 +0900

    정렬 버튼 완성

--- dungreed/static/js/item_js.js
+++ dungreed/static/js/item_js.js
@@ -72,4 +72,45 @@ $(document).ready(function() {
         $("#ipt2").val(vl2);
         $("#slider").slider("values", [vl1,vl2]);
     });
+
+    $(".ordbtn").click(function(){
+        $order("item_list","item_element");
+    });
+
+    $order = function(li, bb) {
+        var list, i, switching, b, shouldSwitch, dir, switchcount = 0;
+        list = document.getElementsByClassName(li)[0];
+        switching = true;
+        dir = "asc"; 
+        while (switching) {
+            switching = false;
+            b = list.getElementsByClassName(bb);
+            for (i = 0; i < (b.length - 1); i++) {
+                shouldSwitch = false;
+                if (dir == "asc") {
+                    if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+                        shouldSwitch = true;
+                        $('.ordbtn').text('△');
+                        break;
+                    }
+                } else if (dir == "desc") {
+                    if (b[i].innerHTML.toLowerCase() < b[i + 1].innerHTML.toLowerCase()) {
+                        shouldSwitch= true;
+                        $('.ordbtn').text('▽');
+                        break;
+                    }
+                }
+            }
+            if (shouldSwitch) {
+                b[i].parentNode.insertBefore(b[i + 1], b[i]);
+                switching = true;
+                switchcount ++;
+            } else {
+                if (switchcount == 0 && dir == "asc") {
+                    dir = "desc";
+                    switching = true;
+                }
+            }
+        }
+    }
 });
